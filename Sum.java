e514bd71d6b92154bea421f747b9d7301e4f4603
Talpa31341 <59111217+Talpa31341@users.noreply.github.com>
2020-01-04 15:18:46 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -6,9 +6,9 @@ public class Sum{
 
 	public static void main(String[] args) 
 	{ 
-		int a = 62; 
-		int b = 18; 
+		int a = 31; 
+		int b = 31; 
 		// It will return the sum of two arguments. 
-		System.out.println("The sum is =" + Integer.sum(a, b)); 
+		System.out.println("The sum is = " + Integer.sum(a, b)); 
 	} 
 } 
